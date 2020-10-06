e1de2d36f843d945b4760a9518e8b7ea17a640c3
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-10-06 15:27:14 -0400

    added score modifier

--- guessTheFlag/ContentView.swift
+++ guessTheFlag/ContentView.swift
@@ -10,15 +10,15 @@ import SwiftUI
 struct ContentView: View {
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
-    
     @State private var showingScore = false
+    @State private var score = 0
     @State private var scoreTitle = ""
-
+    
     var body: some View {
         ZStack  {
             LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
                 .edgesIgnoringSafeArea(.all)
-
+            
             VStack(spacing:30){
                 VStack {
                     Text("Tap the flag of")
@@ -38,14 +38,13 @@ struct ContentView: View {
                             .clipShape(Capsule())
                             .overlay(Capsule().stroke(Color.black, lineWidth: 1))
                             .shadow(color: .black, radius: 2)
-
                     }
                 }
                 Spacer()
             }
         }
         .alert(isPresented: $showingScore) {
-            Alert(title: Text(scoreTitle), message: Text("Your score is ???"), dismissButton: .default(Text("Continue")) {
+            Alert(title: Text(scoreTitle), message: Text("Your score is \(score)"), dismissButton: .default(Text("Continue")) {
                 self.askQuestion()
             })
         }
@@ -53,10 +52,11 @@ struct ContentView: View {
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
             scoreTitle = "Correct"
+            score += 1
         } else {
             scoreTitle = "Wrong"
         }
-
+        
         showingScore = true
     }
     func askQuestion() {
