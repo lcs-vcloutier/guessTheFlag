1855a8d449d864d9181a8e8ba7d926bcc2c7a923
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-10-06 14:54:22 -0400

    zstack color test

--- guessTheFlag/ContentView.swift
+++ guessTheFlag/ContentView.swift
@@ -9,24 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(spacing: 20) {
-            Text("Hello World")
-            HStack(spacing: 20) {
-                Text("Hello World")
-                VStack(spacing: 20) {
-                    Text("Hello World")
-                    HStack(spacing: 20) {
-                        Text("Hello World")
-                    }
-                }
-            }
-
+        ZStack {
+            Color.red.frame(width: 200, height: 200)
+            Text("Your content")
         }
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
