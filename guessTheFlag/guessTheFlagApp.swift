1fa814802a41493843018a492438f99b6b20e6d2
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-10-06 14:41:14 -0400

    Initial Commit

---
+++ guessTheFlag/guessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  guessTheFlagApp.swift
+//  guessTheFlag
+//
+//  Created by Cloutier, Vincent on 2020-10-06.
+//
+
+import SwiftUI
+
+@main
+struct guessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
